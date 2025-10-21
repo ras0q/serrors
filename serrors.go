108ad845e857f695ce05a057961536039ae71cda
ras0q <ras0q@users.noreply.github.com>
2025-10-21 15:47:43 +0900

    refactor: rename files

--- serrors.go
+++ serrors.go
@@ -1,7 +1,9 @@
 package serrors
 
 import (
+	"context"
 	"errors"
+	"log/slog"
 	"strings"
 )
 
@@ -12,6 +14,41 @@ type LogAttrsProvider interface {
 type LogAttrsProviderError interface {
 	error
 	LogAttrsProvider
+}
+
+type Handler struct {
+	slog.Handler
+}
+
+var _ slog.Handler = (*Handler)(nil)
+
+func NewHandler(h slog.Handler) *Handler {
+	return &Handler{h}
+}
+
+// Handle implements slog.Handler.
+func (h *Handler) Handle(ctx context.Context, record slog.Record) error {
+	record.Attrs(func(a slog.Attr) bool {
+		if a.Key != "error" {
+			return true
+		}
+
+		v := a.Value.Any()
+		if v == nil {
+			return true
+		}
+
+		err, ok := v.(LogAttrsProviderError)
+		if !ok {
+			return true
+		}
+
+		record.Add(err.LogAttrs()...)
+
+		return true
+	})
+
+	return h.Handler.Handle(ctx, record)
 }
 
 type structuredError struct {
